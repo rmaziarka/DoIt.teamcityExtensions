3cd2cf70422a450004abfa9a3fad41d5bfe86f21
Konrad Korus <kkorus@objectivity.co.uk>
2015-10-27 13:06:56 +0100

    Added Zap extensions.

---
+++ Zap/Start-ZapSpider.ps1
@@ -0,0 +1,71 @@
+<#
+The MIT License (MIT)
+
+Copyright (c) 2015 Objectivity Bespoke Software Specialists
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+#>
+
+function Start-ZapSpider {
+	<#
+    .SYNOPSIS
+	Starts ZAP Spider for specified url.
+    
+    .PARAMETER Url
+    Url for which Spider should be run.
+    
+    .PARAMETER ApiKey
+    Api key which it was run with ZAP.
+
+    .PARAMETER Interval
+    Time in seconds for which Spider status should be checked.
+
+    .EXAMPLE
+    Start-ZapSpider -Url 'http://localhost:8080' -ApiKey 12345 -Interval 1
+    #>
+    [CmdletBinding()]
+    [OutputType([void])]
+    param(
+		[Parameter(Mandatory=$true)]
+        [string]
+        $Url,
+
+		[Parameter(Mandatory=$false)]
+        [int]
+        $ApiKey = 12345,
+
+		[Parameter(Mandatory=$false)]
+        [int]
+        $Interval = 1
+    )
+
+	$scanUrl = "http://zap/JSON/spider/action/scan/?zapapiformat=JSON&apikey=" + $ApiKey + "&url=" + $Url +"&maxChildren=&recurse="
+	$responseScan = Invoke-WebRequestWrapper -Uri $scanUrl -Method "Get" -ContentType "JSON"
+	$json = $responseScan.Content | ConvertFrom-Json
+	$scanId = $json.scan
+
+	$status = 0
+	while($status -lt 100) {
+		$urlGetStatusUrl = "http://zap/JSON/spider/view/status/?zapapiformat=JSON&scanId=" + $scanId
+		$responseStatus = Invoke-WebRequestWrapper -Uri $urlGetStatusUrl -Method "Get" -ContentType "JSON"
+		$json = $responseStatus.Content | ConvertFrom-Json
+		$status = $json.status
+		Start-Sleep -s $Interval
+	}
+}
