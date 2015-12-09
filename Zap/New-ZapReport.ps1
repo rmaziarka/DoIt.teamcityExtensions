821dd73365b39c6804660204435587ffcf5bd034
unknown <rseniuta@PLRSENIUTA.objectivity.co.uk>
2015-12-09 09:10:31 +0100

    Fixed testFailed instruction in ZAP

--- Zap/New-ZapReport.ps1
+++ Zap/New-ZapReport.ps1
@@ -86,7 +86,7 @@ function New-ZapReport {
         $xmlResponse = Invoke-WebRequestWrapper $xmlReportUrl -Proxy "http://localhost:$Port"
         $alertsCount = ([xml]$xmlResponse.Content).SelectNodes("//alertitem[riskcode>=$MinimalFailureCode]").Count
         if($alertsCount -gt 0) {
-            Write-Output ("##teamcity[testFailed because alerts of risk '{0}' (or higher) were found.]" -f $MinimalFailureThreshold)            
+            Write-Output ("##teamcity[testFailed name='ZAP' message='One or more alerts of risk {0} (or higher) were found.']" -f $MinimalFailureThreshold)
         }
     }
 
